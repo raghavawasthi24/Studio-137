ccf262b22827cee123b3ef734b673eb3c6130b84
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-12-24 12:20:24 +0530

    quiz questions with custom slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Quiz from './pages/Quiz/Quiz';
+import Home from './pages/Home/Home';
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
